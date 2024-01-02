b574037620a486dac1a322d57fc956f69fed0956
swoolee97 <swoolee97@ajou.ac.kr>
2024-01-02 10:16:40 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/glomeet/GlomeetApplication.java
@@ -0,0 +1,13 @@
+package com.example.glomeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlomeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlomeetApplication.class, args);
+    }
+
+}
