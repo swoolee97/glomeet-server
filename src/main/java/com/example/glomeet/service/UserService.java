af620aa6c729d488e8f51b14ace0b28e54265a26
swoolee97 <swoolee97@ajou.ac.kr>
2024-01-06 22:48:36 +0900

    refactor: UserDetails에서 db접근하는 방식으로 변경

--- src/main/java/com/example/glomeet/service/UserService.java
+++ src/main/java/com/example/glomeet/service/UserService.java
@@ -5,18 +5,17 @@ import com.example.glomeet.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class UserService {
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
     private final UserMapper userMapper;
     private final CustomUserDetailsImpl customUserDetails;
     private final PasswordEncoder encoder;
@@ -30,11 +29,7 @@ public class UserService {
     }
 
     public Authentication signIn(UserDTO userDTO) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        UserDTO user = userMapper.findUserByEmail(userDTO.getEmail());
-        if (user == null) {
-            throw new UsernameNotFoundException("존재하지 않는 사용자입니다");
-        }
+        UserDetails user = customUserDetails.loadUserByUsername(userDTO.getEmail());
         if (!encoder.matches(userDTO.getPassword(), user.getPassword())) {
             throw new BadCredentialsException("비밀번호가 일치하지 않습니다");
         }
@@ -43,8 +38,8 @@ public class UserService {
                 userDTO.getEmail(),
                 userDTO.getPassword()
         );
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        return authentication;
+
+        return authenticationToken;
     }
 
     public boolean isValidEmail(String email) {
