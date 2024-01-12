83746df2a998755615cbe6744b16466c4e10ad3e
swoolee97 <swoolee97@ajou.ac.kr>
2024-01-12 17:41:04 +0900

    chore: 불필요한 테스트 코드 삭제

--- src/test/java/com/example/glomeet/GlomeetApplicationTests.java
+++ src/test/java/com/example/glomeet/GlomeetApplicationTests.java
@@ -1,12 +1,7 @@
 package com.example.glomeet;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class GlomeetApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
