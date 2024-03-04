2fb0f554a9722a4d84327324a254803cc1a4280a
swoolee97 <swoolee97@ajou.ac.kr>
2024-03-04 21:48:05 +0900

    feat: 모임 참여하는 기능

--- src/main/java/com/example/glomeet/controller/MeetingController.java
+++ src/main/java/com/example/glomeet/controller/MeetingController.java
@@ -1,9 +1,15 @@
 package com.example.glomeet.controller;
 
 import com.example.glomeet.entity.Meeting;
+import com.example.glomeet.response.Response;
 import com.example.glomeet.service.MeetingService;
+import com.example.glomeet.service.MeetingService.ExceededMeetingCapacityException;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/group")
+@RequestMapping("/meeting")
 public class MeetingController {
 
     private final MeetingService meetingService;
@@ -19,6 +25,23 @@ public class MeetingController {
     @PostMapping("/create")
     public void createMeeting(@RequestBody @Valid Meeting meeting) {
         meetingService.createMeeting(meeting);
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<?> joinMeeting(@RequestBody @Valid MemberJoinRequestDTO memberJoinRequestDTO) {
+        try {
+            meetingService.joinMeeting(memberJoinRequestDTO);
+        } catch (DataIntegrityViolationException e) {
+            return new ResponseEntity<>(new Response("이미 속해있는 모임이에요"), HttpStatus.CONFLICT);
+        } catch (ExceededMeetingCapacityException e) {
+            return new ResponseEntity<>(new Response(e.getMessage()), HttpStatus.CONFLICT);
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @Getter
+    public static class MemberJoinRequestDTO {
+        private String meetingId;
     }
 
 }
--- src/main/java/com/example/glomeet/mapper/MeetingMapper.java
+++ src/main/java/com/example/glomeet/mapper/MeetingMapper.java
@@ -6,4 +6,10 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MeetingMapper {
     void insertMeeting(Meeting meeting);
+
+    int countMeetingUser(String meetingId);
+
+    int findMeetingCapacityById(String meetingId);
+
+    int insertMeetingUser(String meetingId, String email);
 }
